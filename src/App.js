ba0fe0099d2c3fdd99acf1e087c628a3ad102134
Balogh Csaba <35346323+Csabesz1130@users.noreply.github.com>
2024-04-20 12:05:15 +0200

    Updated App.js to use Routes and Route elements from react-router-dom v6.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
 import Inventory from './components/Inventory/InventoryList';
 import StockCounting from './components/StockCounting/StockCounting';
-import OrderList from './components/OrderManagement/OrderList'; // Updated line
-import ReportList from './components/Reporting/ReportList'; // Updated line
+import OrderList from './components/OrderManagement/OrderList';
+import ReportList from './components/Reporting/ReportList';
 import Settings from './components/Settings/Settings';
 import Forecasting from './components/Forecasting/Forecasting';
 
@@ -29,6 +29,33 @@ function App() {
             <li>
               <Link to="/forecasting">Forecasting</Link>
             </li>
+            <li>
+              <Link to="/order-management">Order Management</Link>
+            </li>
+            <li>
+              <Link to="/reporting">Reporting</Link>
+            </li>
+            <li>
+              <Link to="/settings">Settings</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/stock-counting" element={<StockCounting />} />
+          <Route path="/forecasting" element={<Forecasting />} />
+          <Route path="/order-management" element={<OrderList />} />
+          <Route path="/reporting" element={<ReportList />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
             <li>
               <Link to="/order-management">Order Management</Link>
             </li>
